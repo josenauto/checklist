12da9580525739acdcdd943ef10e84608bc1e37f
josenauto <jemilionautoch@gmail.com>
2015-07-24 15:06:50 -0300

    I added the view edit.html.erb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,9 +26,15 @@ class ItemsController < ApplicationController
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+  end
 
   private
 
